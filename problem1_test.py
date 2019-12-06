a42c31da9a10a8bc223e4da3a59a96978b1599a5
unknown <wael.imaginecup@gmail.com>
2019-12-06 18:52:38 +0100

    final work (tested and working)

--- problem1_test.py
+++ problem1_test.py
@@ -11,19 +11,33 @@ import numpy as np
 def zombieCluster(matrix):
     #write your zombieCluster here
     #your function should return an int to be tested
-    def searchConnectedZombies(matrix, lineIndex):
-        print('Hello!')
-        
+    def searchConnectedZombies(matrix, lineIndex, cluster_elements):
+        (_, nCol) = np.shape(matrix)
+        result = cluster_elements
+        for i in range(nCol):
+            # Recursive call for search connected zombies if an element is not already included in the cluster
+            if (matrix[lineIndex][i] == 1) and (lineIndex != i) and (i not in cluster_elements):
+                result.append(i)
+                connectedZombies = searchConnectedZombies(matrix, i, result)
+                result = result + connectedZombies
+        return result
+
     (nLine,nCol) = np.shape(matrix)
     lineIdx = 0
     nClusters = 0
+    clusters_elements = []
     while lineIdx < nLine:
+        clusters_elements.append(lineIdx)
         nClusters +=1
-        clusters_elements = []
+        current_cluster = [lineIdx]
         for colIdx in range(nCol):
-            if (matrix[lineIdx][colIdx] == 1) and (colIdx != colIdx): # make an exception for the diagonal elements
-                clusters_elements = list(set(clusters_elements + searchConnectedZombies(matrix, lineIdx)))
-        # search of an element not included in any cluster
+            # look for all connections of the current zombie, make an exception for the diagonal elements
+            if (matrix[lineIdx][colIdx] == 1) and (colIdx != lineIdx):
+                current_cluster.append(colIdx)
+                current_cluster = current_cluster + searchConnectedZombies(matrix, colIdx, current_cluster)
+                clusters_elements = list(set(clusters_elements + current_cluster))
+        print("Cluter " + str(nClusters) + " = " + str(set(current_cluster)))
+        # search for an element not included in any cluster
         while(lineIdx in clusters_elements):
             lineIdx +=1
     return nClusters
@@ -53,6 +67,7 @@ class test_problem(unittest.TestCase):
         msg = 'wrong value computed. zombieCluster returned {} when expected {}'
         for i in range(self.n_tests):
             computed_res = zombieCluster(self.matrices[i])
+            print("matrix " + str(i+1) + " total clusters= " + str(computed_res))
             with(self.subTest(i=i)):
                 self.assertEqual(computed_res, self.values[i], msg.format(computed_res, self.values[i]))
 
